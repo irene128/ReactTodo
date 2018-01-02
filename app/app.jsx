59d83988010f3fde3b6bf63c622eea5d2595eed3
jialin128 <jialin9112@gmail.com>
2018-01-02 23:03:28 +0800

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,17 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Walk the dog'));
+store.dispatch(actions.setSearchText('dog'));
+store.dispatch(actions.toggleShowCompleted());
+
 //css
 $(document).foundation();
 
