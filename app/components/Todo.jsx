446d5c1aa506ab5a4f8c8d557745f14a45483931
jialin128 <jialin9112@gmail.com>
2018-01-10 23:09:31 +0800

    Refactor TodoList, Todo, and AddTodo to work with redux

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,13 +1,15 @@
 var React = require('react');
+var {connect} = require('react-redux');
 var moment = require('moment');
+var actions = require('actions');
 
-var Todo = React.createClass({
+export var Todo = React.createClass({
   handleClick: function () {
     var {id} = this.props;
     this.props.onToggle(id);
   },
   render: function () {
-  var {id, text, completed, createdAt, completedAt} = this.props;
+  var {id, text, completed, createdAt, completedAt, dispatch} = this.props;
   var todoClassName = completed ? 'todo todo-completed' : 'todo';
   var renderDate = () => {
     var message = 'Created ';
@@ -21,7 +23,9 @@ var Todo = React.createClass({
     return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
     };
     return (
-    <div className={todoClassName} onClick={this.handleClick}>
+    <div className={todoClassName} onClick={() => {
+      dispatch(actions.toggleTodo(id));
+    }}>
     <div>
       <input type="checkbox" checked={completed}/>
     </div>
@@ -34,4 +38,6 @@ var Todo = React.createClass({
   }
 });
 
-module.exports = Todo;
+//module.exports = Todo;
+//module.exports = connect()(Todo);
+export default connect()(Todo);
--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -1,27 +1,37 @@
 var React = require('react');
-var Todo = require('Todo');
+var {connect} = require('react-redux'); //specify which children they like
+import Todo from 'Todo';
 
-var TodoList = React.createClass({
+export var TodoList = React.createClass({
   render: function () {
     var {todos} = this.props;
     var renderTodos = () => {
-    if (todos.length === 0){
-      return (
-        <p className="container__message">Nothing To Do</p>
-      );
-    }
+      if (todos.length === 0) {
+        return (
+          <p className="container__message">Nothing To Do</p>
+        );
+      }
+
       return todos.map((todo) => {
         return (
-          <Todo key={todo.id} {...todo} onToggle={this.props.onToggle}/>
+          <Todo key={todo.id} {...todo}/>
         );
-      })
-    }
+      });
+    };
+
     return (
-    <div>
-      {renderTodos()}
-    </div>
-    );
+      <div>
+        {renderTodos()}
+      </div>
+    )
   }
 });
 
-module.exports = TodoList;
+//module.exports = TodoList;
+export default connect(
+  (state) => {
+    return {
+      todos: state.todos
+    };
+  }
+)(TodoList);
