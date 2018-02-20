4bd6781f661af2aeb3a0097041741a71049b2ecd
jialin128 <jialin9112@gmail.com>
2018-02-20 21:48:29 +0800

    Move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,18 +1,17 @@
 import firebase from 'firebase';
-
+//console.log("API_KEY  " +process.env.API_KEY);
 try {
   var config = {
-      apiKey: "AIzaSyCCqKAq_d4B0XQRmJoUqUwD8UjtJwOFOpk",
-      authDomain: "jialin-todo-app.firebaseapp.com",
-      databaseURL: "https://jialin-todo-app.firebaseio.com",
-      projectId: "jialin-todo-app",
-      storageBucket: "jialin-todo-app.appspot.com",
-      messagingSenderId: "382459944014"
-    };
-    firebase.initializeApp(config);
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+  };
+
+  firebase.initializeApp(config);
 } catch (e) {
 
 }
 
-  export var firebaseRef = firebase.database().ref();
-  export default firebase;
+export var firebaseRef = firebase.database().ref();
+export default firebase;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 var webpack = require('webpack');
 var path = require('path');
+var envFile = require('node-env-file');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+try {
+  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
+} catch (e) {
+
+}
 
 module.exports = {
   entry: [
@@ -9,17 +16,26 @@ module.exports = {
     'script!foundation-sites/dist/foundation.min.js',
     './app/app.jsx'
   ],
-  externals:{
+  externals: {
     jquery: 'jQuery'
   },
-  Plugins:[
+  plugins: [
     new webpack.ProvidePlugin({
       '$': 'jquery',
-      'jquery': 'jquery'
+      'jQuery': 'jquery'
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET)
       }
     })
   ],
@@ -48,7 +64,7 @@ module.exports = {
       {
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015','stage-0']
+          presets: ['react', 'es2015', 'stage-0']
         },
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/
@@ -56,9 +72,9 @@ module.exports = {
     ]
   },
   sassLoader: {
-   includePaths: [
-     path.resolve(__dirname, './node_modules/foundation-sites/scss')
-   ]
- },
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
+    ]
+  },
   devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 };
